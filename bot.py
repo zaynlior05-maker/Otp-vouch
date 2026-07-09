719e693c1b79b53e25fee9ce675584cbdfc56a73
zaynlior05-maker <zaynlior05@gmail.com>
2026-07-10 00:04:26 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -689,3 +689,4 @@ if __name__ == "__main__":
 #   with TelegramClient(StringSession(), api_id, api_hash) as client:
 #       print(client.session.save())
 # ============================================================================
+ 
